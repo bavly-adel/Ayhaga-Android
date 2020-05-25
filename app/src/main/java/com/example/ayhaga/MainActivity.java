3ee7f48cb5d481ea9b7946d674fc69480bf5937f
bavly adel <bavllyadell1998@gmail.com>
2020-05-25 05:08:30 +0200

    forget password screen

--- app/src/main/java/com/example/ayhaga/MainActivity.java
+++ app/src/main/java/com/example/ayhaga/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.ayhaga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button forgetPassBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        forgetPassBtn = (Button) findViewById(R.id.forgetPassBtn);
+        forgetPassBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForgetPasswordActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
