5205d220ddb04e9284d12d60d6a9c2ee759238f0
bavly adel <bavllyadell1998@gmail.com>
2020-06-20 22:22:47 +0200

    phase 2 test

--- app/src/main/java/com/ctg/ayhaga/BooksActivity.java
+++ app/src/main/java/com/ctg/ayhaga/BooksActivity.java
@@ -1,7 +1,11 @@
 package com.ctg.ayhaga;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,26 +21,141 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class BooksActivity extends AppCompatActivity {
 
+    AppCompatEditText childNameTxt;
+    AlertDialog.Builder builder;
+    ProgressBar progressBar;
+
+    LinearLayoutCompat latestBokks;
+    LinearLayoutCompat booksParentLayout;
+
+    TextView latestTxt;
+
+
     RecyclerView recyclerView;
     List<Book> books;
-    private static String JSON_URL = "https://dashboard.ayhaga.app/api/books";
+    private static String JSON_URL = "https://dashboard.ayhaga.app/api/books/";
     BooksRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+        builder = new AlertDialog.Builder(this);
+
+        latestBokks = findViewById(R.id.latestBooks);
+        booksParentLayout = findViewById(R.id.booksParentLayout);
+
+        latestTxt = findViewById(R.id.latestTxt);
+        progressBar = findViewById(R.id.bookProgress);
+
+        progressBar.setVisibility(View.INVISIBLE);
 
         books = new ArrayList<>();
         recyclerView = findViewById(R.id.booksList);
+        childNameTxt = findViewById(R.id.childNameTxt);
+
+        findViewById(R.id.searchBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open right drawer
+                books.clear();
+                progressBar.setVisibility(View.VISIBLE);
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                }, 2000);
+                getBookByName(childNameTxt.getText().toString());
+
+
+            }
+        });
 
         getBooks();
+
+    }
+
+    private void getBookByName(String name){
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, JSON_URL+name, null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+
+                    if(response.length() >0) {
+
+                        JSONObject booksObject = response.getJSONObject(0);
+                        Book book = new Book();
+                        book.setName(booksObject.getString("name").toString());
+                        book.setFile_path(booksObject.getString("file_path").toString());
+                        book.setId(Integer.parseInt(booksObject.getString("id").toString()));
+
+                        //System.out.println("=- =-= -="+book.getFile_path()+" -= -= -= -"+book.getName()+"= -=-="+book.getId()+" -= -=-= -= -= - " + i);
+
+                        books.add(book);
+
+                        latestTxt.setText("");
+                    }else {
+
+                        latestTxt.setText("الإسم غير متاح حالياً، تابعنا على مدار اليوم وهيتم اضافته فى أقرب وقت ");
+//                        builder.setMessage("الإسم غير متاح حالياً، تابعنا على مدار اليوم وهيتم اضافته فى أقرب وقت ")
+//                                .setCancelable(false)
+//                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                                    public void onClick(DialogInterface dialog, int id) {
+//                                        finish();
+//                                        Toast.makeText(getApplicationContext(),"you choose yes action for alertbox",
+//                                                Toast.LENGTH_SHORT).show();
+//                                    }
+//                                })
+//                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                                    public void onClick(DialogInterface dialog, int id) {
+//                                        //  Action for 'NO' Button
+//                                        dialog.cancel();
+//                                        Toast.makeText(getApplicationContext(),"you choose no action for alertbox",
+//                                                Toast.LENGTH_SHORT).show();
+//                                    }
+//                                });
+//                        //Creating dialog box
+//                        AlertDialog alert = builder.create();
+//                        //Setting the title manually
+//                        alert.setTitle("AlertDialogExample");
+//                        alert.show();
+                    }
+
+                } catch (JSONException e) {
+                    System.out.println("Errooooooooooor ");
+                    e.printStackTrace();
+                }
+
+
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                adapter = new BooksRecyclerAdapter(getApplicationContext(),books);
+                recyclerView.setAdapter(adapter);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("tag", "onErrorResponse: " + error.getMessage());
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+        //progressBar.setVisibility(View.INVISIBLE);
 
     }
 
